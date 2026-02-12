7171a25be54c6308cfba3b4b85bea533d0facec3
Ugur Bozkaya <ugur@stackav.com>
2026-02-12 21:30:05 +0000

    througput helpers

---
+++ cosmos/throughput.py
@@ -0,0 +1,101 @@
+"""Inference script."""
+
+from pathlib import Path
+from typing import Annotated
+import json
+import os
+import sys
+import pydantic
+import tyro
+from cosmos_oss.init import cleanup_environment, init_environment, init_output_dir
+from cosmos_predict2.inference import Inference
+from cosmos_predict2.config import (
+    InferenceArguments,
+    InferenceOverrides,
+    SetupArguments,
+    handle_tyro_exception,
+    is_rank0,
+)
+
+import os
+
+def get_nproc_per_node():
+    # 'LOCAL_WORLD_SIZE' environment variable holds the value passed to --nproc-per-node
+    if 'LOCAL_WORLD_SIZE' in os.environ:
+        try:
+            return int(os.environ['LOCAL_WORLD_SIZE'])
+        except ValueError:
+            print("Error: LOCAL_WORLD_SIZE is not a valid integer.")
+    else:
+        # Fallback or handle cases where the script is not launched via torchrun
+        print("Warning: LOCAL_WORLD_SIZE environment variable not found. Returning None.")
+        return -1
+
+
+class Args(pydantic.BaseModel):
+    model_config = pydantic.ConfigDict(extra="forbid", frozen=True)
+
+    ###input_files: Annotated[list[Path], tyro.conf.arg(aliases=("-i",))]
+    """Path to the inference parameter file(s).
+    If multiple files are provided, the model will be loaded once and all the samples will be run sequentially.
+    """
+    setup: SetupArguments
+    """Setup arguments. These can only be provided via CLI."""
+    overrides: InferenceOverrides
+    """Inference parameter overrides. These can either be provided in the input json file or via CLI. CLI overrides will overwrite the values in the input file."""
+
+import os
+def main(
+    args: Args,
+):
+    inference_type = args.overrides.inference_type
+    inference_samples = []
+    pai_data = json.load(open("/datasets/physical-ai-bench-generation/cosmos_predict2_bench_full_info.json", "r"))
+    for item in pai_data:
+        video_id = item["video_id"]
+        prompt_en = item["prompt_en"]
+        if inference_type == "text2world":
+            inference_sample = InferenceArguments(name=video_id, prompt=prompt_en, inference_type="text2world", num_output_frames=33, num_steps=35, seed=0, guidance=4)
+            inference_samples.append(inference_sample)
+        elif inference_type == "image2world":
+            image_path = f"/datasets/physical-ai-bench-generation/condition_image/{video_id}.jpg"
+            inference_sample = InferenceArguments(input_path=image_path, name=video_id, prompt=prompt_en, inference_type="image2world", num_output_frames=33, num_steps=35, seed=0, guidance=4)
+            inference_samples.append(inference_sample)
+        else:
+            raise ValueError(f"Unsupported inference type: {inference_type}")
+
+
+    import time
+    init_output_dir(args.setup.output_dir, profile=args.setup.profile)
+    inference = Inference(args.setup)
+
+    start_time = time.perf_counter()
+    inference.generate(inference_samples[0:1], output_dir=args.setup.output_dir)
+    elapsed_time = time.perf_counter() - start_time
+    if is_rank0():
+        print(f"\n\nSTEP 2 Warmup generate() Elapsed time: {elapsed_time:.4f} seconds", get_nproc_per_node())
+
+    for i in range(3):
+        start_time = time.perf_counter()
+        inference.generate(inference_samples[i:i+1], output_dir=args.setup.output_dir)
+        elapsed_time = time.perf_counter() - start_time
+        if is_rank0():
+            print(f"\n\nSTEP 3 generate Elapsed time: {elapsed_time:.4f} seconds", get_nproc_per_node())
+
+
+if __name__ == "__main__":
+    init_environment()
+
+    try:
+        args = tyro.cli(
+            Args,
+            description=__doc__,
+            console_outputs=is_rank0(),
+            config=(tyro.conf.OmitArgPrefixes,),
+        )
+    except Exception as e:
+        handle_tyro_exception(e)
+    # pyrefly: ignore  # unbound-name
+    main(args)
+
+    cleanup_environment()
---
+++ cosmos/throughput.sh
@@ -0,0 +1,62 @@
+source $HOME/.local/bin/env
+
+# Check if HF_TOKEN is set and not empty
+if [[ -z "$HF_TOKEN" ]]; then
+  echo "HF_TOKEN is either unset or empty"
+fi
+
+
+if [ $# -eq 0 ]; then
+    echo "Error: Please provide an inference type [text2world|image2world]"
+    exit 1
+fi
+
+inference_type=$1
+if [ "$inference_type" != "text2world" ] && [ "$inference_type" != "image2world" ]; then
+    echo "Error: Invalid inference type. Must be 'text2world' or 'image2world'"
+    exit 1
+fi
+echo "Inference type: $inference_type"
+
+gpu_model=$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader,nounits -i 0)
+echo "Detected GPU: $gpu_model"
+
+available_gpus=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+echo "Available GPUs: $available_gpus"
+if [ $available_gpus -lt 4 ]; then
+    echo "Error: At least 4 NVIDIA GPUs are required"
+    exit 1
+fi
+
+
+# Get the absolute path of the directory containing this script
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "The script is located in: ${ROOT_DIR}"
+
+
+echo  "Running inference and benchmarking for inference type: $inference_type"
+
+
+
+# Run inference using custom pyton script mounted from repo
+cd /cosmos-predict2.5
+
+if [ "$GPU_MODEL" == "NVIDIA GB200" ]; then
+    uv sync --python 3.10 --extra=cu130   > /dev/null 2>&1  || true
+else
+    uv sync --python 3.10 --extra=cu128   > /dev/null 2>&1  || true
+fi
+source .venv/bin/activate
+
+#torchrun --nproc_per_node=$available_gpus "/$ROOT_DIR/throughput.py" --inference-type $inference_type --disable-guardrails -o "/tmp"
+
+#torchrun --nproc_per_node=8 "/$ROOT_DIR/throughput.py" --inference-type $inference_type --disable-guardrails -o "/tmp"
+#torchrun --nproc_per_node=4 "/$ROOT_DIR/throughput.py" --inference-type $inference_type --disable-guardrails -o "/tmp"
+#torchrun --nproc_per_node=2 "/$ROOT_DIR/throughput.py" --inference-type $inference_type --disable-guardrails -o "/tmp"
+#torchrun --nproc_per_node=1 "/$ROOT_DIR/throughput.py" --inference-type $inference_type --disable-guardrails -o "/tmp"
+
+python $ROOT_DIR/throughput.py --inference-type $inference_type --disable-guardrails -o "/tmp"
+
+deactivate
+
+
