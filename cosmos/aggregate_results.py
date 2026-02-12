bd1f60ca044c6127580bd1b74fac5a0decee14bb
Ugur Bozkaya <ugur@stackav.com>
2026-02-12 19:11:04 +0000

    fix domain/quality score bug and add overall score

--- cosmos/aggregate_results.py
+++ cosmos/aggregate_results.py
@@ -15,28 +15,36 @@ def main():
     print(f'Inference type: {args.inference_type}')
 
 
-    # find the domain results file
-    domain_input_file = glob.glob(f'{args.input_dir}/*_eval_results.json')[0]
-    domain_results =json.load(open(domain_input_file))
+    # The Quality Score is derived from eight text-to-video and image-to-video metrics adapted from VBench.
+    # Find the quality results file
+    quality_input_file = glob.glob(f'{args.input_dir}/*_eval_results.json')[0]
+    quality_results =json.load(open(quality_input_file))
     #ignore image to video scores for text2world
     if args.inference_type == 'text2world':
-        domain_results = {k:v[0] for k,v in domain_results.items() if not k.startswith('i2v_')}
+        quality_results = {k:v[0] for k,v in quality_results.items() if not k.startswith('i2v_')}
     elif args.inference_type == 'image2world':
-        domain_results = {k:v[0] for k,v in domain_results.items()} 
+        quality_results = {k:v[0] for k,v in quality_results.items()} 
     else:
         raise ValueError(f'Unknown inference type: {args.inference_type}')
 
-    domain_scores = domain_results.values()
-    domain_average_score = sum(domain_scores) / len(domain_scores)
+    quality_scores = quality_results.values()
+    quality_average_score = sum(quality_scores) / len(quality_scores)
 
-    quality_input_file = f'{args.input_dir}/vqa_summary.json'
-    quality_results = json.load(open(quality_input_file))
-    quality_score = quality_results['overall_accuracy']
+    # The Domain Score is obtained through VQA-based evaluation across seven domains: av, common,
+    # human, industry, misc, physics, and robotics.
+    # Find the domain results file
+    domain_input_file = f'{args.input_dir}/vqa_summary.json'
+    domain_results = json.load(open(domain_input_file))
+    domain_score = domain_results['overall_accuracy']
+
+    # The final PAI-Bench Overall Score is computed as the average of the Quality and Domain scores.
+    overall_score = (quality_average_score + domain_score) / 2
 
     results = f"""
 {"="*13} Final Results {"="*13} 
-Domain  score {domain_average_score:.4f}
-Quality score {quality_score:.4f}
+Domain  score {domain_score:.3f}
+Quality score {quality_average_score:.3f}
+Overall score {overall_score:.3f}
     """
     print(results)
 
