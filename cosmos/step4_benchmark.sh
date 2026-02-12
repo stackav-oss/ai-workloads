65efbd5f3186613c212e5c6a11d57f9bd1da3dae
Ugur Bozkaya <ugur@stackav.com>
2026-02-12 17:46:36 +0000

    debugging

--- cosmos/step4_benchmark.sh
+++ cosmos/step4_benchmark.sh
@@ -42,7 +42,7 @@ uv pip install --no-build-isolation "git+https://github.com/facebookresearch/det
 if [ "$gpu_model" == "NVIDIA GB200" ]; then
     echo "Using PyTorch with CUDA 13.0 for NVIDIA GB200"
     uv pip install --reinstall torch torchvision --index-url https://download.pytorch.org/whl/cu130
-    v pip install --reinstall numpy==1.26.4
+    uv pip install --reinstall numpy==1.26.4
 fi
 
 python -m torch.utils.collect_env | grep "PyTorch"
