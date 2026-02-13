027ad19ca7fb113653602b61bcdfea9bf8fbbdae
Ugur Bozkaya <ugur@stackav.com>
2026-02-13 14:24:43 +0000

    debugging

--- cosmos/throughput.sh
+++ cosmos/throughput.sh
@@ -50,12 +50,14 @@ source .venv/bin/activate
 
 #torchrun --nproc_per_node=$available_gpus "/$ROOT_DIR/throughput.py" --inference-type $inference_type --disable-guardrails -o "/tmp"
 
-#torchrun --nproc_per_node=8 "/$ROOT_DIR/throughput.py" --inference-type $inference_type --disable-guardrails -o "/tmp"
-#torchrun --nproc_per_node=4 "/$ROOT_DIR/throughput.py" --inference-type $inference_type --disable-guardrails -o "/tmp"
-#torchrun --nproc_per_node=2 "/$ROOT_DIR/throughput.py" --inference-type $inference_type --disable-guardrails -o "/tmp"
-#torchrun --nproc_per_node=1 "/$ROOT_DIR/throughput.py" --inference-type $inference_type --disable-guardrails -o "/tmp"
+if [ $available_gpus -gt 4 ]; then
+    torchrun --nproc_per_node=8 "/$ROOT_DIR/throughput.py" --inference-type $inference_type --disable-guardrails -o "/tmp"
+fi
+torchrun --nproc_per_node=4 "/$ROOT_DIR/throughput.py" --inference-type $inference_type --disable-guardrails -o "/tmp"
+torchrun --nproc_per_node=2 "/$ROOT_DIR/throughput.py" --inference-type $inference_type --disable-guardrails -o "/tmp"
+torchrun --nproc_per_node=1 "/$ROOT_DIR/throughput.py" --inference-type $inference_type --disable-guardrails -o "/tmp"
 
-python $ROOT_DIR/throughput.py --inference-type $inference_type --disable-guardrails -o "/tmp"
+#python $ROOT_DIR/throughput.py --inference-type $inference_type --disable-guardrails -o "/tmp"
 
 deactivate
 
