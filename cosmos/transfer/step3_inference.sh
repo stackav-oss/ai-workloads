c86368765dbe8807ac0b4578c6d4052690628138
Ugur Bozkaya <ugur@stackav.com>
2026-03-11 15:01:06 +0000

    cosmos transport support wip

--- cosmos/transfer/step3_inference.sh
+++ cosmos/transfer/step3_inference.sh
@@ -60,6 +60,6 @@ else
 fi
 source .venv/bin/activate
 
-python -m torch.distributed.run --standalone --nproc_per_node=$available_gpus --master_port=12341 "/$ROOT_DIR/inference.py" --disable-guardrails -o "/results/transfer/$control_type/inference" control:$control_type
+torchrun --nproc_per_node=$available_gpus --master_port=12341 "/$ROOT_DIR/inference.py" --disable-guardrails -o "/results/transfer/$control_type/inference" control:$control_type
 
 deactivate
